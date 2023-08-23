4489a527e1187d258dd33e8ca55fd6d891421f4b
adrianjose01 <dejesuseusebbio21@hotmail.com>
2023-08-23 17:54:04 -0400

    adding dbconfig

---
+++ server/dbconfig.js
@@ -0,0 +1,11 @@
+module.exports = dbconfig = {
+  db: {
+    host: "adma.mysql.database.azure.com",
+    user: "adma",
+    password: "Jose8745!",
+    database: "adma",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
