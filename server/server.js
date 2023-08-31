8885bd17602d8d3da142fe5f7400fa4bcf1f57c0
adrianjose01 <dejesuseusebbio21@hotmail.com>
2023-08-30 20:50:55 -0400

    testing setinterval

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const query = require("./dbHelpers/query");
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(
@@ -15,6 +16,14 @@ app.use(cors());
 const admaRoutes = require("./routes/admaRoutes");
 
 app.use(admaRoutes);
+
+setInterval(async () => {
+  await query(
+    `INSERT INTO messages(message, created_at) VALUES ('MENSAJE ENVIADO', NOW());`
+  ).then((res) => {
+    console.log("Mensaje Enviado");
+  });
+}, 10000);
 
 const PORT = 5000;
 
