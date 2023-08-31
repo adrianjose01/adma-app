a7f6d4547bb1176e9c27b345980df8a2c6bf2709
adrianjose01 <dejesuseusebbio21@hotmail.com>
2023-08-30 20:58:07 -0400

    deleting setinterval

--- server/server.js
+++ server/server.js
@@ -17,14 +17,6 @@ const admaRoutes = require("./routes/admaRoutes");
 
 app.use(admaRoutes);
 
-setInterval(async () => {
-  await query(
-    `INSERT INTO messages(message, created_at) VALUES ('MENSAJE ENVIADO', NOW());`
-  ).then((res) => {
-    console.log("Mensaje Enviado");
-  });
-}, 10000);
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
